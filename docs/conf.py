f54e05f5b85f03ca7440b57a7598b46e6832d80b
mahesh <mahesh.cn@outlook.com>
2017-12-27 21:05:13 +0530

    Custom CSS changes
    Custom CSS changes

--- docs/conf.py
+++ docs/conf.py
@@ -109,7 +109,11 @@ todo_include_todos = True
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_context = {
+    'css_files': ['_static/custom.css'],
+}
 
+html_logo = '_assets/logo.png';
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #

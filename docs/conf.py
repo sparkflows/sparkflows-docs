df827688f01f24e43588f620b24b634f05e6ed1c
mahesh <mahesh.cn@outlook.com>
2017-12-27 21:51:14 +0530

    test

--- docs/conf.py
+++ docs/conf.py
@@ -34,11 +34,7 @@ html_theme = "sphinx_rtd_theme"
 
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-html_context = {
-    'css_files': ['_static/custom.css'],
-}
 
-html_logo = '_assets/logo.png';
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -114,7 +110,11 @@ todo_include_todos = True
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+# html_context = {
+#     'css_files': ['_static/custom.css'],
+# }
 
+html_logo = '_assets/logo.png';
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #

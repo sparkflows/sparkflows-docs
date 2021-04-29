b736cf3e8fe00efd9ee3f2cf140c394a52b8c2e1
jayantshekhar <jayant_s11@yahoo.com>
2021-04-29 16:14:33 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,7 @@ todo_include_todos = True
 # further.  For a list of options available for each theme, see the
 # documentation.
 # https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html#confval-sticky_navigation
-html_theme_options = {'sticky_navigation': False}
+html_theme_options = {'sticky_navigation': False, 'titles_only': True}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

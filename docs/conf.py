2e8beb1a64d204a687bd89175f7daf2d8679197b
SparkflowsGaurav <106712495+SparkflowsGaurav@users.noreply.github.com>
2022-09-01 14:46:44 -0700

    Added autosectionlabel
    Added sphinx.ext.autosectionlabel

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx_panels']
+    'sphinx_panels',
+    'sphinx.ext.autosectionlabel']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

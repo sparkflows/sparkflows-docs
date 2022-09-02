2dcce86149cfbff90f40a5d74cbf148f9b227f0b
SparkflowsGaurav <106712495+SparkflowsGaurav@users.noreply.github.com>
2022-09-02 15:58:42 -0700

    removed autosection label
    autosection label broke build so removed and looking for a fix.

--- docs/conf.py
+++ docs/conf.py
@@ -48,8 +48,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx_panels',
-    'sphinx.ext.autosectionlabel']
+    'sphinx_panels']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

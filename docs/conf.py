b2d4165d37654c7fca16ce5fe3b4855f29c7d01e
Kundan <kundankumar@nmbrsystems.com>
2024-09-05 17:07:17 +0530

    added build_compatibility extention for old build behaviour

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,9 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx.ext.autosectionlabel',
-    'sphinx_panels']
+    'sphinx_panels',
+    'sphinx_build_compatibility.extension'
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
